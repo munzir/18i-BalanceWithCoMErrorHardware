c73a0c5b7de5d691ea31e739a6052f8ebb0f6075
Munzir Zafar <zafarmunzir@gmail.com>
2018-09-22 00:11:46 -0400

    separated adrc functions. not fully implemented yet

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -37,7 +37,7 @@ Vector6d K = K_groundLo;
 
 
 const double wheelRadius = 10.5; 							///< Radius of krang wheels in inches
-const double distanceBetweenWheels = 27.375; 	///< Distance Between krang's wheels in inches 
+const double distanceBetweenWheels = 27.375; 	///< Distance Between krang's wheels in inches
 
 /* ******************************************************************************************** */
 Eigen::MatrixXd fix (const Eigen::MatrixXd& mat) {
@@ -51,27 +51,27 @@ Eigen::MatrixXd fix (const Eigen::MatrixXd& mat) {
 /* ******************************************************************************************** *
 // Setup the indices for the motor groups
 
-int left_arm_ids_a [7] = {11, 13, 15, 17, 19, 21, 23}; 
-int right_arm_ids_a [7] = {12, 14, 16, 18, 20, 22, 24}; 
+int left_arm_ids_a [7] = {11, 13, 15, 17, 19, 21, 23};
+int right_arm_ids_a [7] = {12, 14, 16, 18, 20, 22, 24};
 int imuWaist_ids_a [3] = {5, 8};
-vector <int> left_arm_ids (left_arm_ids_a, left_arm_ids_a + 7);						
-vector <int> right_arm_ids (right_arm_ids_a, right_arm_ids_a + 7);	
-vector <int> imuWaist_ids (imuWaist_ids_a, imuWaist_ids_a + 2);		
+vector <int> left_arm_ids (left_arm_ids_a, left_arm_ids_a + 7);
+vector <int> right_arm_ids (right_arm_ids_a, right_arm_ids_a + 7);
+vector <int> imuWaist_ids (imuWaist_ids_a, imuWaist_ids_a + 2);
 
 // Needed to temporarily adjust the torso misaligment
 int imuWaistTorso_ids_a [3] = {5, 8, 9};
-vector <int> imuWaistTorso_ids (imuWaistTorso_ids_a, imuWaistTorso_ids_a + 3);		
+vector <int> imuWaistTorso_ids (imuWaistTorso_ids_a, imuWaistTorso_ids_a + 3);
 /* ******************************************************************************************** */
 
 /* ******************************************************************************************** */
-/// Get the joint values from the encoders and the imu and compute the center of mass as well 
-void getState(Vector6d& state, double dt, Vector3d* com_) {
+/// Get the joint values from the encoders and the imu and compute the center of mass as well
+void getState(Vector6d& state, double dt, Vector3d* com_, SkeletonPtr rob) {
 
 	// Read motor encoders, imu and ft and update dart skeleton
   krang->updateSensors(dt);
 
-	// Calculate the COM	
-  Vector3d com = robot->getCOM() - robot->getPositions().segment(3,3);
+	// Calculate the COM
+  Vector3d com = rob->getCOM() - rob->getPositions().segment(3,3);
 //	Vector3d com = Eigen::Vector3d::Zero();
 //	com(2) -= 0.264;
 //	com(0) += 0.008;
@@ -90,6 +90,7 @@ void getState(Vector6d& state, double dt, Vector3d* com_) {
 	com(0) = com(2) * tan(state(0));
 }
 
+
 /* ******************************************************************************************** */
 /// Update reference left and right wheel pos/vel from joystick data where dt is last iter. time
 void updateReference (double js_forw, double js_spin, double dt, Vector6d& refState) {
@@ -107,23 +108,23 @@ void updateReference (double js_forw, double js_spin, double dt, Vector6d& refSt
 }
 
 /* ******************************************************************************************** */
-/// Returns the values of axes 1 (left up/down) and 2 (right left/right) in the joystick 
+/// Returns the values of axes 1 (left up/down) and 2 (right left/right) in the joystick
 bool getJoystickInput(double& js_forw, double& js_spin) {
 
 	// Get the message and check output is OK.
 	int r = 0;
-	Somatic__Joystick *js_msg = 
+	Somatic__Joystick *js_msg =
 			SOMATIC_GET_LAST_UNPACK( r, somatic__joystick, NULL /*&protobuf_c_system_allocator*/, 4096, &js_chan );
 	if(!(ACH_OK == r || ACH_MISSED_FRAME == r) || (js_msg == NULL)) return false;
 
 	// Get the values
-	for(size_t i = 0; i < 10; i++) 
+	for(size_t i = 0; i < 10; i++)
 		b[i] = js_msg->buttons->data[i] ? 1 : 0;
 	memcpy(x, js_msg->axes->data, sizeof(x));
 
 	// Free the joystick message
 	somatic__joystick__free_unpacked(js_msg, NULL /*&protobuf_c_system_allocator*/);
-	
+
 	// Change the gains with the given joystick input
 	double deltaTH = 0.2, deltaX = 0.02, deltaSpin = 0.02;
 	if(!joystickControl) {
@@ -137,10 +138,10 @@ bool getJoystickInput(double& js_forw, double& js_spin) {
 		// Reset fts
 	if(!resetLeftFT && (x[4] < -0.5)) resetLeftFT = true;
 	if(!resetRightFT && (x[4] > 0.5)) resetRightFT = true;
-	
+
 	// Update joystick and force-compensation controls
 	static int lastb0 = b[0], lastb1 = b[1], lastb2 = b[2];
-	if((b[4] == 1) && (b[6] == 0) && (b[0] == 1) && (lastb0 == 0)) { 
+	if((b[4] == 1) && (b[6] == 0) && (b[0] == 1) && (lastb0 == 0)) {
 		joystickControl = !joystickControl;
 		if(joystickControl == true) {
 			somatic_motor_reset(&daemon_cx, krang->arms[LEFT]);
@@ -150,19 +151,19 @@ bool getJoystickInput(double& js_forw, double& js_spin) {
 	if((b[4] == 1) && (b[6] == 0) && (b[1] == 1) && (lastb1 == 0)) complyTorque = !complyTorque;
 	if((b[4] == 1) && (b[6] == 0) && (b[2] == 1) && (lastb2 == 0)) {
 		if(MODE == 4) {
-			printf("Mode 5\n"); 
+			printf("Mode 5\n");
 			K = K_balHigh;
 			MODE = 5;
 		}
 		else if (MODE == 5) {
-			printf("Mode 4\n"); 
+			printf("Mode 4\n");
 			K = K_balLow;
 			MODE = 4;
 		}
 	}
 	lastb0 = b[0], lastb1 = b[1], lastb2 = b[2];
 
-	// Ignore the joystick statements for the arm control 
+	// Ignore the joystick statements for the arm control
 	if((b[4] == 1) || (b[5] == 1) || (b[6] == 1) || (b[7] == 1)) {
 		js_forw = js_spin = 0.0;
 		return true;
@@ -181,11 +182,11 @@ bool getJoystickInput(double& js_forw, double& js_spin) {
 	}
 	else {
 		js_forw = -x[1] * jsFwdAmp;
-		js_spin = x[2] * jsSpinAmp;; 
+		js_spin = x[2] * jsSpinAmp;;
 	}
 
-	
-		
+
+
 	return true;
 }
 
@@ -229,12 +230,12 @@ void readGains () {
 /// Sets a global variable ('start') true if the user presses 's'
 void *kbhit(void *) {
 	char input;
-	while(true){ 
-		input=cin.get(); 
-		if(input=='s') start = true; 
+	while(true){
+		input=cin.get();
+		if(input=='s') start = true;
 		else if(input=='t') complyTorque = !complyTorque;
-		else if(input=='e') resetLeftFT = true; 
-		else if(input=='r') resetRightFT = true; 
+		else if(input=='e') resetLeftFT = true;
+		else if(input=='r') resetRightFT = true;
 		else if(input=='.') readGains();
 		else if(input=='q') overwriteFT = true;
 		else if(input=='p') spinFT = true;
@@ -242,7 +243,7 @@ void *kbhit(void *) {
 		else if(input==']') spinGoal += 5.0;
 		else if(input=='{') downGoal -= 5.0;
 		else if(input=='}') downGoal += 5.0;
-		else if(input=='j') { 
+		else if(input=='j') {
 			joystickControl = !joystickControl;
 			if(joystickControl == true) {
 				somatic_motor_reset(&daemon_cx, krang->arms[LEFT]);
@@ -250,32 +251,32 @@ void *kbhit(void *) {
 			}
 		}
 		else if(input=='1') {
-			printf("Mode 1\n"); 
+			printf("Mode 1\n");
 			K = K_groundLo;
 			MODE = 1;
 		}
 		else if(input=='2') {
-			printf("Mode 2\n"); 
+			printf("Mode 2\n");
 			K = K_stand;
 			MODE = 2;
 		}
 		else if(input=='3') {
-			printf("Mode 3\n"); 
+			printf("Mode 3\n");
 			K = K_sit;
 			MODE = 3;
 		}
 		else if(input=='4') {
-			printf("Mode 4\n"); 
+			printf("Mode 4\n");
 			K = K_balLow;
 			MODE = 4;
 		}
 		else if(input=='5') {
-			printf("Mode 5\n"); 
+			printf("Mode 5\n");
 			K = K_balHigh;
 			MODE = 5;
 		}
 		else if(input=='6') {
-			printf("Mode 6\n"); 
+			printf("Mode 6\n");
 			K = K_groundHi;
 			MODE = 6;
 		}
@@ -288,7 +289,7 @@ void *kbhit(void *) {
 
 /* ********************************************************************************************* */
 /// Computes the imu value from the imu readings
-void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt, 
+void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 		filter_kalman_t* kf) {
 
 	// ======================================================================
@@ -299,14 +300,14 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 	struct timespec currTime;
 	clock_gettime(CLOCK_MONOTONIC, &currTime);
 	struct timespec abstime = aa_tm_add(aa_tm_sec2timespec(1.0/30.0), currTime);
-	Somatic__Vector *imu_msg = SOMATIC_WAIT_LAST_UNPACK(r, somatic__vector, 
+	Somatic__Vector *imu_msg = SOMATIC_WAIT_LAST_UNPACK(r, somatic__vector,
 			/*&protobuf_c_system_allocator*/ NULL, IMU_CHANNEL_SIZE, imuChan, &abstime );
 	assert((imu_msg != NULL) && "Imu message is faulty!");
 
 	// Get the imu position and velocity value from the readings (note imu mounted at 45 deg).
 	static const double mountAngle = -.7853981634;
 	double newX = imu_msg->data[0] * cos(mountAngle) - imu_msg->data[1] * sin(mountAngle);
-	_imu = atan2(newX, imu_msg->data[2]); 
+	_imu = atan2(newX, imu_msg->data[2]);
 	_imuSpeed = imu_msg->data[3] * sin(mountAngle) + imu_msg->data[4] * cos(mountAngle);
 
 	// Free the unpacked message
@@ -332,7 +333,7 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 	kf->R[1] = (dt*dt*dt) * k1 * (1.0 / 2.0);
 	kf->R[2] = (dt*dt*dt) * k1 * (1.0 / 2.0);
 	kf->R[3] = (dt*dt) * k1b;
-	
+
 	// First make a prediction of what the reading should have been, then correct it
 	filter_kalman_predict(kf);
 	filter_kalman_correct(kf);
@@ -342,13 +343,13 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 }
 
 /* ******************************************************************************************** */
-/*void computeExternal (const Vector6d& input, SkeletonDynamics& robot, Vector6d& external, 
+/*void computeExternal (const Vector6d& input, SkeletonDynamics& robot, Vector6d& external,
 		bool left) {
 
 	// Get the point transform wrench due to moving the affected position from com to sensor origin
 	// The transform is an identity with the bottom left a skew symmetric of the point translation
-	Matrix6d pTcom_sensor = MatrixXd::Identity(6,6); 
-	pTcom_sensor.bottomLeftCorner<3,3>() << 0.0, -s2com(2), s2com(1), s2com(2), 0.0, -s2com(0), 
+	Matrix6d pTcom_sensor = MatrixXd::Identity(6,6);
+	pTcom_sensor.bottomLeftCorner<3,3>() << 0.0, -s2com(2), s2com(1), s2com(2), 0.0, -s2com(0),
 		-s2com(1), s2com(0), 0.0;
 
 	// Get the rotation between the world frame and the sensor frame by setting the arm values
@@ -358,9 +359,9 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 //	vector <int> dofs;
 //	for(size_t i = 0; i < 25; i++) dofs.push_back(i);
 //	if(myDebug) cout << "\nq in computeExternal: " << robot.getConfig(dofs).transpose() << endl;
-	
+
 	// Create the wrench with computed rotation to change the frame from the world to the sensor
-	Matrix6d pSsensor_world = MatrixXd::Identity(6,6); 
+	Matrix6d pSsensor_world = MatrixXd::Identity(6,6);
 	pSsensor_world.topLeftCorner<3,3>() = Rsw;
 	pSsensor_world.bottomRightCorner<3,3>() = Rsw;
 
@@ -368,27 +369,27 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 	// static const double eeMass = 0.169;	// kg - ft extension
 	Vector6d weightVector_in_world;
 	weightVector_in_world << 0.0, 0.0, -eeMass * 9.81, 0.0, 0.0, 0.0;
-	
-	// Compute what the force and torque should be without any external values by multiplying the 
-	// position and rotation transforms with the expected effect of the gravity 
+
+	// Compute what the force and torque should be without any external values by multiplying the
+	// position and rotation transforms with the expected effect of the gravity
 	Vector6d wrenchWeight = pTcom_sensor * pSsensor_world * weightVector_in_world;
 
 	// Remove the effect from the sensor value and convert the wrench into the world frame
 	external = input - wrenchWeight;
-	external = pSsensor_world.transpose() * external;	
+	external = pSsensor_world.transpose() * external;
 }*/
 
 /* ******************************************************************************************** */
-/*void computeOffset (double imu, double waist, const somatic_motor_t& lwa, const Vector6d& raw, 
+/*void computeOffset (double imu, double waist, const somatic_motor_t& lwa, const Vector6d& raw,
 		SkeletonDynamics& robot, Vector6d& offset, bool left) {
 
 	// Get the point transform wrench due to moving the affected position from com to sensor origin
 	// The transform is an identity with the bottom left a skew symmetric of the point translation
-	Matrix6d pTcom_sensor = MatrixXd::Identity(6,6); 
-	pTcom_sensor.bottomLeftCorner<3,3>() << 0.0, -s2com(2), s2com(1), s2com(2), 0.0, -s2com(0), 
+	Matrix6d pTcom_sensor = MatrixXd::Identity(6,6);
+	pTcom_sensor.bottomLeftCorner<3,3>() << 0.0, -s2com(2), s2com(1), s2com(2), 0.0, -s2com(0),
 		-s2com(1), s2com(0), 0.0;
 
-	// Get the rotation between the world frame and the sensor frame. 
+	// Get the rotation between the world frame and the sensor frame.
 	robot.setConfig(imuWaist_ids, Vector2d(-imu + M_PI_2, waist));
 	robot.setConfig(left ? left_arm_ids : right_arm_ids, Map <Vector7d> (lwa.pos));
 	const char* nodeName = left ? "lGripper" : "rGripper";
@@ -399,16 +400,16 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 	cout << "\nq in computeExternal: " << robot.getConfig(dofs).transpose() << endl;
 
 	// Create the wrench with computed rotation to change the frame from the bracket to the sensor
-	Matrix6d pSsensor_bracket = MatrixXd::Identity(6,6); 
+	Matrix6d pSsensor_bracket = MatrixXd::Identity(6,6);
 	pSsensor_bracket.topLeftCorner<3,3>() = R;
 	pSsensor_bracket.bottomRightCorner<3,3>() = R;
-	
+
 	// Get the weight vector (note that we use the bracket frame for gravity so towards -y)
 	Vector6d weightVector_in_bracket;
 	weightVector_in_bracket << 0.0, 0.0, -eeMass * 9.81, 0.0, 0.0, 0.0;
-	
-	// Compute what the force and torque should be without any external values by multiplying the 
-	// position and rotation transforms with the expected effect of the gravity 
+
+	// Compute what the force and torque should be without any external values by multiplying the
+	// position and rotation transforms with the expected effect of the gravity
 	Vector6d expectedFT = pTcom_sensor * pSsensor_bracket * weightVector_in_bracket;
 	pv(raw);
 	pv(expectedFT);
@@ -421,15 +422,15 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 /* ******************************************************************************************* */
 // Compute the wrench on the wheel as an effect of the wrench acting on the sensor
 /*void computeWheelWrench(const Vector6d& wrenchSensor, SkeletonDynamics& robot, Vector6d& wheelWrench, bool left) {
-	
+
 	// Get the position vector of the sensor with respect to the wheels
 	const char* nodeName = left ? "lGripper" : "rGripper";
 	Vector3d Tws = robot.getNode(nodeName)->getWorldTransform().topRightCorner<3,1>();
 	if(0 && myDebug) cout << Tws.transpose() << endl;
 
 	// Get the wrench shift operator to move the wrench from sensor origin to the wheel axis
-	// TODO: This shifting is to the origin of the world frame in dart. It works now because it is not 
-	// being updated by the amc encoders. We need to shift the wrench to a frame having origin at the 
+	// TODO: This shifting is to the origin of the world frame in dart. It works now because it is not
+	// being updated by the amc encoders. We need to shift the wrench to a frame having origin at the
 	// wheel axis.
 	Tws(2) -= 0.264;
 	Matrix6d pTsensor_wheel = MatrixXd::Identity(6,6);
@@ -447,7 +448,7 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 	int result;
 	size_t numBytes = 0;
 	struct timespec abstimeout = aa_tm_future(aa_tm_sec2timespec(.001));
-	uint8_t* buffer = (uint8_t*) somatic_d_get(&daemon_cx, &ft_chan, &numBytes, &abstimeout, 
+	uint8_t* buffer = (uint8_t*) somatic_d_get(&daemon_cx, &ft_chan, &numBytes, &abstimeout,
 		ACH_O_LAST, &result);
 
 	// Return if there is nothing to read
@@ -459,10 +460,10 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 	if(msg->meta->type != SOMATIC__MSG_TYPE__FORCE_MOMENT) return false;
 
 	// Read the force-torque message and write it into the vector
-	Somatic__ForceMoment* ftMessage = somatic__force_moment__unpack(&(daemon_cx.pballoc), 
+	Somatic__ForceMoment* ftMessage = somatic__force_moment__unpack(&(daemon_cx.pballoc),
 		numBytes, buffer);
-	for(size_t i = 0; i < 3; i++) data(i) = ftMessage->force->data[i]; 
-	for(size_t i = 0; i < 3; i++) data(i+3) = ftMessage->moment->data[i]; 
+	for(size_t i = 0; i < 3; i++) data(i) = ftMessage->force->data[i];
+	for(size_t i = 0; i < 3; i++) data(i+3) = ftMessage->moment->data[i];
 	return true;
 }*/
 
